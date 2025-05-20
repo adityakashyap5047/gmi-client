07eaf5ff2611bc10a22e4b9249ecf973cc7c57f3
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-20 13:00:00 +0530

    enhancing the ui

--- src/components/app/LiveTracking.jsx
+++ src/components/app/LiveTracking.jsx
@@ -5,6 +5,24 @@ import { motion } from "framer-motion";
 
 const socket = io(import.meta.env.VITE_PUBLIC_API_URL);
 
+const userLocation = [
+  {
+    email: "a@a.com",
+    lat: 34,
+    lng: 89
+  },
+  {
+    email: "b@b.com",
+    lat: 23,
+    lng: 90
+  },
+  {
+    email: "c@c.com",
+    lat: 12,
+    lng: 54
+  }
+]
+
 export default function LiveTracking() {
   const { user } = useUser();
   const [userLocations, setUserLocations] = useState([]);
@@ -55,19 +73,17 @@ export default function LiveTracking() {
   }, []);
 
   return (
+    <>
     <div className="p-6 mb-12 bg-white/10 backdrop-blur-md rounded-xl shadow-lg border border-white/20">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold mr-4">Live User Coordinates</h2>
-        <button
-          className={`px-4 py-2 rounded text-white ${
-            visible ? "bg-green-600" : "bg-gray-400"
-          }`}
-          onClick={toggleVisibility}
-        >
-          {visible ? "Visible" : "Hidden"}
-        </button>
+        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+          <button onClick={toggleVisibility} className={`${visible ? "bg-cyan-500 hover:bg-cyan-800" : "bg-amber-300 hover:bg-amber-800"} cursor-pointer text-black px-8 py-3 rounded-md font-semibold shadow-md transition duration-300`}>
+            {visible ? "Hide" : "Show"} Your Appearance
+          </button>
+        </motion.div>
       </div>
-      <div className="flex gap-4 flex-wrap">
+      <div className="flex p-4 gap-4 flex-wrap">
         {userLocations.length > 0 && (
           userLocations.map((u) => (
             <motion.section
@@ -82,9 +98,30 @@ export default function LiveTracking() {
               <p><strong>Longitude: </strong>{u.location.lng}</p>
             </motion.section>
           ))
-        )
-        }
+        )}
+        <motion.section
+        initial={{ y: 30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.2 }}
+        className="relative z-10 flex gap-4 bg-white/10 backdrop-blur-md p-4 rounded-xl max-w-3xl text-center shadow-lg border border-white/20 "
+      >
+        {userLocation.map((u) => (
+          <motion.section
+          key={u.email}
+        initial={{ y: 30, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.2 }}
+        className="relative z-10  bg-white/10 backdrop-blur-md p-12 rounded-md max-w-3xl text-center shadow-lg border border-white/20 mb-4"
+      >
+        <p><strong>Email: </strong>{u.email.slice(0, 20)}{u.email.length > 20 && "..."}</p>
+        <p><strong>Latitude: </strong>{u.lat}</p>
+        <p><strong>Longitude: </strong>{u.lng}</p>
+      </motion.section>
+        ))}
+      </motion.section>
       </div>
     </div>
+      
+      </>
   );
 }
